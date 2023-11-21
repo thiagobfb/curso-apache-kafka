56382f2fab201d5e8b8afc0e268e20de9d98ebe5
Thiago Barbosa <thiago.fbarbosa@grupomateus.com>
2023-11-21 20:49:22 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/thiagobernardo/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagobernardo.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

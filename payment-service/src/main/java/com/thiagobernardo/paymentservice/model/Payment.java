9582d381f7aa48fee2b3c718c81aafe14c439635
Thiago Barbosa <thiago.fbarbosa@grupomateus.com>
2024-01-11 22:57:15 -0300

    Implement JSON Producer

--- payment-service/src/main/java/com/thiagobernardo/paymentservice/model/Payment.java
+++ payment-service/src/main/java/com/thiagobernardo/paymentservice/model/Payment.java
@@ -2,8 +2,10 @@ package com.thiagobernardo.paymentservice.model;
 
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class Payment {
+public class Payment implements Serializable {
 
     private Long id;
     private Long idUser;
